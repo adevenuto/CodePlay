e6adcbbf6a2abca4c2967e4a89bc23a51f1b4398
Anthony <anthonydevenuto@gmail.com>
2016-12-19 21:14:41 -0600

    added jquery ui, panel control functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,45 @@
-$('#panel-controls button').on('click', function(){
-  $(this).toggleClass('active');
+var controls = {
+  init: function() {
+    this.cacheDom();
+    this.bindEvent();
+  },
+  cacheDom: function() {
+    this.$controls = $('#panel-controls');
+    this.$panels = $('#code-panels');
+    this.$button = this.$controls.find('button');
+  },
+  bindEvent: function() {
+    this.$button.on('click', this.toggleState.bind(this))
+  },
+  toggleState: function(e) {
+    var $panel = e.target.innerHTML.toLowerCase();
+    var $targetPanel = "#"+$panel+"-panel";
+    $($targetPanel).toggleClass('hide');
+    $(e.target).toggleClass('active');
+  }
+}
+controls.init();
+
+
+
+
+
+
+$(".resize").resizable({
+  handles: 'e',
+  resize: function(e, ui) {
+  // console.log(e.target.clientWidth)
+  }
+});
+
+
+var max = 0;
+
+$(".panel").resize(function(){
+  max = $('#html-panel').width() +
+  $('#css-panel').width() +
+  $('#js-panel').width();
+
+  console.log(max)
 })
-console.log($('panels').length)
+

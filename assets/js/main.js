233ff6d075e913e4b4592bd8737d103751b80d59
Anthony <anthonydevenuto@gmail.com>
2016-12-21 16:21:54 -0600

    connected events to inputs object

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,34 +38,52 @@ $(".resize").resizable({
    }
 });
 /////////////////////////////////////////////////////////
-var input = {
-  init: function() {
-    this.cacheDom();
-  },
-  cacheDom: function() {
-    this.$htmlIn = $('#html-input');
-    this.$htmlIn = $('#css-input');
-    this.$htmlIn = $('#js-input');
-  }
-}
-// input.init();
 var output = {
   init: function() {
     this.cacheDom();
-    this.bindEvent();
+
   },
   cacheDom: function() {
     this.$display = $('#output');
     this.$head = $('#output').find('head');
     this.$content = $('#output').find('body');
-  },
-  bindEvent: function() {
-
   }
 }
 output.init();
+
+var input = {
+  init: function() {
+    this.cacheDom();
+    this.bindEvent();
+  },
+  cacheDom: function() {
+    this.$panels = $('#panels-container');
+    this.$htmlIn = this.$panels.find('#html-input');
+    this.$cssIn = this.$panels.find('#css-input');
+    this.$jsIn = this.$panels.find('#js-input');
+  },
+  bindEvent: function() {
+    this.$htmlIn.on('keyup paste', this.htmlOutput.bind(this))
+    this.$cssIn.on('keyup paste', this.cssOutput.bind(this))
+    this.$jsIn.on('keyup paste', this.jsOutput.bind(this))
+  },
+  htmlOutput: function() {
+    console.log("html");
+  },
+  cssOutput: function() {
+    console.log("css");
+  },
+  jsOutput: function() {
+    console.log("js");
+  }
+}
+input.init();
+
+
+
+
 // $("#textarea").on('change keyup paste', function() {
 //     // your code here
 // });
-$('#output').contents().find('body').html("<h1>Hello</h1>")
-$('#output').contents().find('head').append("<style> h1 { color: red } </style>")
+// $('#output').contents().find('body').html("<h1>Hello</h1>")
+// $('#output').contents().find('head').append("<style> h1 { color: red } </style>")

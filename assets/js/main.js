9d4faa477a4f8d95e102bb865260c6c1d8405fda
Anthony <anthonydevenuto@gmail.com>
2016-12-20 22:16:18 -0600

    adding in/out functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,27 +19,53 @@ var controls = {
   }
 }
 controls.init();
-
-
-
-
-
-
+///////////////////////////////////////////////////
 $(".resize").resizable({
   handles: 'e',
-  resize: function(e, ui) {
-  // console.log(e.target.clientWidth)
-  }
+  minWidth: 20,
+  start:function(){
+     this.other = $(this).next();
+     this.startWidth = this.other.width();
+  },
+  resize:function(e,ui) {
+     var minWidth = ui.element.resizable("option", "minWidth");
+     var diffW = ui.size.width-ui.originalSize.width;
+     if (diffW > this.startWidth-minWidth){
+         diffW =  this.startWidth;
+         ui.size.width = ui.originalSize.width+diffW-minWidth;
+     }
+     this.other.width( Math.max(50, this.startWidth - diffW)  );
+   }
 });
+/////////////////////////////////////////////////////////
+var input = {
+  init: function() {
+    this.cacheDom();
+  },
+  cacheDom: function() {
+    this.$htmlIn = $('#html-input');
+    this.$htmlIn = $('#css-input');
+    this.$htmlIn = $('#js-input');
+  }
+}
+// input.init();
+var output = {
+  init: function() {
+    this.cacheDom();
+    this.bindEvent();
+  },
+  cacheDom: function() {
+    this.$display = $('#output');
+    this.$head = $('#output').find('head');
+    this.$content = $('#output').find('body');
+  },
+  bindEvent: function() {
 
-
-var max = 0;
-
-$(".panel").resize(function(){
-  max = $('#html-panel').width() +
-  $('#css-panel').width() +
-  $('#js-panel').width();
-
-  console.log(max)
-})
-
+  }
+}
+output.init();
+// $("#textarea").on('change keyup paste', function() {
+//     // your code here
+// });
+$('#output').contents().find('body').html("<h1>Hello</h1>")
+$('#output').contents().find('head').append("<style> h1 { color: red } </style>")
